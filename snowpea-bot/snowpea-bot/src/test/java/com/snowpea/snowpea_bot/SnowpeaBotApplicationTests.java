8f33a0977ee9561786ee9d3a318124ece401fc3c
Ian Kim <ianhkim@umich.edu>
2025-01-05 18:07:03 -0800

    Added Start Files
    Springboot, among other things

---
+++ snowpea-bot/snowpea-bot/src/test/java/com/snowpea/snowpea_bot/SnowpeaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowpea.snowpea_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowpeaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
